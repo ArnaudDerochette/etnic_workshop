273bf42fe58760a5e06b8bf7fcce3aa7f85eebc5
Dimitri Obin <dimitri.obin@anais.digital>
2021-06-24 18:42:47 +0200

    finish the solution for modals and form

--- README.md
+++ README.md
@@ -6,19 +6,21 @@ Cette branche sert de correctif à l'exercice.
 
 Une breve analyse qui permet de voir que les points suivants ne sont pas accessibles.
 
--   [ ] Le menu
+-   [x] Le menu
     -   [x] Ajouter un 'skip to content'
-    -   [ ] Le burger menu n'est pas focusable, on ne peut pas ouvrir le menu au clavier
-    -   [ ] Ajouter une indiquation dans le menu pour connaitre la page courrante
+    -   [x] Le burger menu n'est pas focusable, on ne peut pas ouvrir le menu au clavier
+    -   [x] Aria expanded sur le burger
 -   [x] Le focus n'est pas visible.
--   [ ] les contrastes de couleurs ne sont pas assez élevés
 -   [x] la hiérarchie des titres n'est pas cohérente
--   [ ] landmarks pas suffisants
+-   [x] landmarks pas suffisants
+-   [x] La partie 'EXPERT TRAINERS' contient des fenêtres modales.
+    -   [x] Ces fenêtres doivent pouvoir boucler le focus, c'est à dire que lorsqu'on est sur le dernier élément de la modale et qu'on tabule pour passer au suivant, le focus doit revenir au premier et non pas sortir de la modale.
+    -   [x] Un utilisateur de clavier dois pouvoir quitter facilement la modale, en général avec la touche ECHAP
+    -   [x] Lorsqu'on zoom, on perd la possibilité de scroller dans la modale.
+    -   [x] Le reste du DOM ne dois pas être navigable (ni scrollable) quand la modale est active
+-   [ ] Formulaire de contact
+    -   [ ] Mettre des labels liés au inputs
+    -   [ ] Mettre une phrase pour expliquer que les champs avec une astérisque sont obligatoires
+-   [ ] les contrastes de couleurs ne sont pas assez élevés
 -   [ ] Les noms ne sont pas remplis pour certains liens/images/...
--   [ ] La partie 'OUR CLASSES' n'est pas formattée sous forme d'onglets
--   [ ] La partie 'EXPERT TRAINERS' contient des fenêtres modales.
-    -   [ ] Ces fenêtres doivent pouvoir boucler le focus, c'est à dire que lorsqu'on est sur le dernier élément de la modale et qu'on tabule pour passer au suivant, le focus doit revenir au premier et non pas sortir de la modale.
-    -   [ ] Un utilisateur de clavier dois pouvoir quitter facilement la modale, en général avec la touche ECHAP
-    -   [ ] La taille des icones est trop petite pour des utilisateurs avec des problèmes de vue.
-    -   [ ] Lorsqu'on zoom, on perd la possibilité de scroller dans la modale.
-    -   [ ] Le reste du DOM ne dois pas être navigable (ni scrollable) quand la modale est active
+-   [ ] La taille des icones est trop petite pour des utilisateurs avec des problèmes de vue.
--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -1,64 +1,149 @@
 const sectionTrainers = document.querySelector("#trainers");
 const trainersCards = sectionTrainers.querySelectorAll(".trainer-item");
 
+class Modal {
+    constructor(element) {
+        this.previousActiveElement = document.activeElement;
+
+        this.element = element;
+        this.elementLink = this.element.querySelector(".image-thumb");
+
+        this.modal = this.build(this.element);
+
+        this.setFocusable();
+
+        this.modal
+            .querySelector("button.close")
+            .addEventListener("click", this.close);
+
+        window.addEventListener("click", (e) => {
+            if (e.target == this.modal) {
+                this.close(e);
+            }
+        });
+
+        document.addEventListener("keydown", this.onKeyDown);
+
+        this.trap(true);
+
+        this.firstFocusable.focus();
+    }
+
+    build = (element) => {
+        const contentElements = element.querySelectorAll(".down-content > *");
+        const thumbnail = element.querySelector(".image-thumb img");
+
+        const modal = document.createElement("div");
+        modal.classList.add("modal", "d-block", "bg-dark");
+
+        modal.innerHTML += `
+        <div class="modal-dialog modal-dialog-scrollable">
+        <div class="modal-content d-flex flex-column justify-content-center align-items-center">
+        <button type="button" class="close align-self-end p-3" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+        <div class="modal-body d-flex flex-column justify-content-around align-items-center">
+        </div>
+        </div>
+        </div>
+        `;
+
+        if (thumbnail) {
+            modal.querySelector(
+                ".modal-body"
+            ).innerHTML += `<img src="${thumbnail.src}" class="w-100">`;
+        }
+
+        contentElements.forEach((element) => {
+            modal.querySelector(".modal-body").innerHTML += element.outerHTML;
+        });
+
+        if (modal.querySelector(".social-icons")) {
+            modal
+                .querySelector(".social-icons")
+                .classList.add(
+                    "d-flex",
+                    "justify-content-between",
+                    "align-items-center",
+                    "w-50",
+                    "p-5"
+                );
+        }
+
+        document.body.appendChild(modal);
+
+        return modal;
+    };
+
+    setFocusable = () => {
+        this.focusable = this.modal.querySelectorAll(
+            'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+        );
+        this.firstFocusable = this.focusable[0];
+        this.lastFocusable = this.focusable[this.focusable.length - 1];
+    };
+
+    onKeyDown = (e) => {
+        switch (e.key) {
+            case "Escape":
+                this.close(e);
+                break;
+
+            case "Tab":
+                this.loop(e);
+                break;
+
+            default:
+                break;
+        }
+    };
+
+    loop = (e) => {
+        //Rotate Focus
+        if (e.shiftKey && document.activeElement === this.firstFocusable) {
+            e.preventDefault();
+            this.lastFocusable.focus();
+        } else if (
+            !e.shiftKey &&
+            document.activeElement === this.lastFocusable
+        ) {
+            e.preventDefault();
+            this.firstFocusable.focus();
+        }
+    };
+
+    trap = (bool) => {
+        Array.from(document.body.children).forEach((child) => {
+            if (child !== this.element) {
+                if (bool) {
+                    child.setAttribute("aria-hidden", true);
+                } else {
+                    child.removeAttribute("aria-hidden");
+                }
+            }
+        });
+    };
+
+    close = (e) => {
+        console.log(this.modal);
+        e.preventDefault();
+
+        this.trap(false);
+
+        this.previousActiveElement.focus();
+
+        document.removeEventListener("keydown", this.onKeyDown);
+
+        this.modal.parentElement.removeChild(this.modal);
+    };
+}
+
 trainersCards.forEach((card) => {
     const cardLink = card.querySelector(".image-thumb");
 
     cardLink.addEventListener("click", (e) => {
         e.preventDefault();
 
-        let modal = createModal(card);
-
-        modal.querySelector("button.close").addEventListener("click", (e) => {
-            e.preventDefault();
-            document.body.removeChild(modal);
-        });
-
-        window.addEventListener("click", (e) => {
-            if (e.target == modal) {
-                document.body.removeChild(modal);
-            }
-        });
+        new Modal(card);
     });
 });
-
-const createModal = (element) => {
-    const contentElements = element.querySelectorAll(".down-content > *");
-    const thumbnail = element.querySelector(".image-thumb img");
-
-    const modal = document.createElement("div");
-    modal.classList.add("modal", "d-block", "bg-dark");
-
-    console.log(contentElements);
-
-    modal.innerHTML += `
-        <div class="modal-dialog">
-            <div class="modal-content d-flex flex-column justify-content-center align-items-center">
-                <button type="button" class="close align-self-end p-3" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                <div class="modal-body d-flex flex-column justify-content-around align-items-center">
-                    <img src="${thumbnail.src}" class="w-100">
-                </div>
-            </div>
-        </div>
-    `;
-
-    contentElements.forEach((element) => {
-        modal.querySelector(".modal-body").innerHTML += element.outerHTML;
-    });
-
-    modal
-        .querySelector(".social-icons")
-        .classList.add(
-            "d-flex",
-            "justify-content-between",
-            "align-items-center",
-            "w-50",
-            "p-5"
-        );
-    modal.querySelector("h3").classList.add("my-3");
-    document.body.appendChild(modal);
-
-    return modal;
-};
